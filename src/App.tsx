9f1e0fff7d66233c05c4bd150bf212735c790cd8
Pedro Saikoski <jptsaikoski@gmail.com>
2023-09-13 19:11:53 -0300

    Shadcn UI instalado e configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-function App() {
+import { Button } from "./components/ui/button";
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <Button>Teste</Button>
   )
 }
-
-export App
